0b55ab160ec4791d4f6e90eda160944f9b532c12
alexlovesquadrupeds <43828864+alexlovesquadrupeds@users.noreply.github.com>
2018-10-26 23:51:10 -0700

    update oct 26
    location image files uploaded

---
+++ SOSCSRPG/ClassLibrary1/Factories/WorldFactory.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1.PlayerModels;
+
+namespace ClassLibrary1.Factories
+{
+    internal class WorldFactory
+    {
+        internal World CreateWorld()
+        {
+            World newWorld = new World();
+            
+            return new World();
+
+            newWorld.AddLocation(-2, -1, "The Great Deku Tree",
+                "The Great Deku Tree is sad. You sense that something is wrong...",
+                "/ClassLibrary1;component/Images/Locations/deku.png");
+
+            newWorld.AddLocation(-1, 0, "The Lost Woods",
+                "Welcome. Try not to get lost",
+                "/ClassLibrary1;component/Images/Locations/lostWoods.jpg");
+
+            newWorld.AddLocation(0, 0, "Home",
+                "This is your house. Get up!",
+                "/ClassLibrary1;component/Images/Locations/home.png");
+
+            newWorld.AddLocation(-1, -1, "Kokiri Forest",
+                "You head to the Kokiri Store. Purveyors of fine goods.",
+                "/ClassLibrary1;component/Images/Locations/kokiri.png");
+
+            newWorld.AddLocation(0, 1, "Hyrule Field",
+                "You see the open plains of Hyrule Field",
+                "/ClassLibrary1;component/Images/Locations/lostWoods.jpg");
+
+            newWorld.AddLocation(1, 1, "Zora's River",
+                "This beautiful stream leads to Zora's domain",
+                "/ClassLibrary1;component/Images/Locations/zorasRiver.png");
+
+            newWorld.AddLocation(2, 1, "Zora's Domain",
+                "Home to the Zora family and Lord Jabu-Jabu",
+                "/ClassLibrary1;component/Images/Locations/zoraDomain.png");
+
+            newWorld.AddLocation(0, 2, "Castle Town",
+                "The main hub and center of activity in the Kingdom, where Hyruleans come to engage in business and trade goods.",
+                "/ClassLibrary1;component/Images/Locations/castleTown.jpg");
+
+            newWorld.AddLocation(0, 3, "Hyrule Castle",
+                "The home of the Princess Zelda and the Royal Family of Hyrule",
+                "/ClassLibrary1;component/Images/Locations/hyruleCastle.jpg");
+
+
+        }
+    }
+}
--- SOSCSRPG/ClassLibrary1/ViewModels/GameSession.cs
+++ SOSCSRPG/ClassLibrary1/ViewModels/GameSession.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ClassLibrary1.PlayerModels;
+using ClassLibrary1.Factories;
 
 namespace ClassLibrary1.ViewModels
 {
     public class GameSession
-    {
+    {      
+        public World CurrentWorld { get; set; }
         public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
 
         public GameSession()
         {
@@ -21,6 +24,17 @@ namespace ClassLibrary1.ViewModels
             CurrentPlayer.Gold = 1000000;
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
+
+            CurrentLocation = new Location();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.XCoordinate = 0;
+            CurrentLocation.YCoordinate = 0;
+            CurrentLocation.Description = "This is your house. Get up!";
+            CurrentLocation.ImageName = "/ClassLibrary1;component/Images/Locations/home.png";
+
+            WorldFactory factory = new WorldFactory();
+            CurrentWorld = factory.CreateWorld();
+
         }
     }
 }
